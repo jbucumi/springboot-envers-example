e82751dc03d8473266ab538a0cbe0f098205c615
Jean Claude BUCUMI <jeanclaude.bucumi@obr.gov.bi>
2021-05-28 08:43:34 +0200

    Init project

---
+++ src/main/java/tech/inkware/springenvers/SpringEnversApplication.java
@@ -0,0 +1,13 @@
+package tech.inkware.springenvers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEnversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEnversApplication.class, args);
+	}
+
+}
